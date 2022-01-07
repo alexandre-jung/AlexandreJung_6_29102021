959cff4eca79f946a0f0eb52aee0d2ad1754c08a
Alexandre Jung <jungalexandre@ymail.com>
2022-01-07 18:39:26 +0100

    :adhesive_bandage: fix document onload
    - Prevent flash of unstyled content
    - Ensure DOM is loaded before executing javascript

--- src/js/main.mjs
+++ src/js/main.mjs
@@ -4,37 +4,41 @@ import DataFetcher from './api.mjs';
 import Router from './router.mjs';
 import routes from './routes.mjs';
 
-const contentDataFetcher = new DataFetcher('/api/data.json');
+function main() {
+    const contentDataFetcher = new DataFetcher('/api/data.json');
 
-// create the router and its paths
-const router = new Router(contentDataFetcher);
-routes.forEach(function (route) {
-    router.add(route);
-});
+    // create the router and its paths
+    const router = new Router(contentDataFetcher);
+    routes.forEach(function (route) {
+        router.add(route);
+    });
 
-function runRouter(href) {
-    const url = new URL(href);
-    const path = url.pathname + url.search;
-    history.pushState({}, '', path);
-    router.run(path);
+    function runRouter(href) {
+        const url = new URL(href);
+        const path = url.pathname + url.search;
+        history.pushState({}, '', path);
+        router.run(path);
+    }
+
+    // delegate navigation events to the document
+    document.addEventListener('click', function (ev) {
+        if (ev.target.nodeName.toLowerCase() == 'a' &&
+            ev.target.rel == 'tag') {
+            ev.preventDefault();
+            runRouter(ev.target.href);
+        } else if (ev.target.hasAttribute('data-navlink')) {
+            const link = ev.target.closest('[rel=tag]');
+            ev.preventDefault();
+            runRouter(link.href);
+        }
+    });
+
+    // run router when navigating back and forward
+    window.addEventListener('popstate', function (ev) {
+        router.run(window.location.pathname, ev.state);
+    });
+    // run router on page load
+    runRouter(location);
 }
 
-// delegate navigation events to the document
-document.addEventListener('click', function (ev) {
-    if (ev.target.nodeName.toLowerCase() == 'a' &&
-        ev.target.rel == 'tag') {
-        ev.preventDefault();
-        runRouter(ev.target.href);
-    } else if (ev.target.hasAttribute('data-navlink')) {
-        const link = ev.target.closest('[rel=tag]');
-        ev.preventDefault();
-        runRouter(link.href);
-    }
-});
-
-// run router when navigating back and forward
-window.addEventListener('popstate', function (ev) {
-    router.run(window.location.pathname, ev.state);
-});
-// run router on page load
-runRouter(location);
+window.addEventListener('load', main);
